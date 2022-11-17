d7e44de3c3a5cd9667f4e01cc1412e2f885dda8d
Мария <m.415796@gmail.com>
2022-11-17 17:32:53 +0300

    Update header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,14 +6,27 @@ export default function Navbar() {
         <nav className="navbar navbar-expand navbar-dark bg-dark">
             <div className="header_container">
                 <div>
-                    <a className="navbar-nav mr-auto nav-link" href="#">
-                        Button
-                    </a>
+                    <ul className="navbar-nav mr-auto">
+                        <li>
+                            <a className="nav-link" href="/home">Home</a>
+                        </li>
+                        <li>
+                            <a className="nav-link" href="#">Link</a>
+                        </li>
+                        <li>
+                            <a className="nav-link" href="#">
+                                Login
+                            </a>
+                        </li>
+                    </ul>
                 </div>
+
                 <div>
-                    <a className="navbar-nav mr-auto nav-link" href="#">
-                        Login
-                    </a>
+                    <ul className="navbar-nav mr-auto">
+                        <li>
+                            <a className="nav-link" href="#">Login</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
